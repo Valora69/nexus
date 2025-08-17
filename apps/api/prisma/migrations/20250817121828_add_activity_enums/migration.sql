becf1a36e7bf2e11fc25d39cc17e6a758e3019fc
sidadrian3 <sidadrian3@gmail.com>
2025-08-17 20:21:50 +0800

    feat: added create and update activity dto and added enums

---
+++ apps/api/prisma/migrations/20250817121828_add_activity_enums/migration.sql
@@ -0,0 +1,5 @@
+-- CreateEnum
+CREATE TYPE "public"."ActivityNameEnum" AS ENUM ('CREATED', 'UPDATED', 'DELETED');
+
+-- CreateEnum
+CREATE TYPE "public"."ActivityOnEnum" AS ENUM ('PROJECT_DETAILS', 'PAYMENT', 'EXPENSE');
