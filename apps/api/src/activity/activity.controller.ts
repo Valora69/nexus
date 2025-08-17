b872cc9adc8ed16cf8f384e0b8ac8c38f476f301
sidadrian3 <sidadrian3@gmail.com>
2025-08-17 20:48:43 +0800

    feat(api): add activity management with event emitter and new migration scripts

--- apps/api/src/activity/activity.controller.ts
+++ apps/api/src/activity/activity.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ActivitiesService } from './activity.service';
+import { CreateActivityDto } from './dto/create-activity.dto';
 
 @Controller('activity')
-export class ActivityController {}
+export class ActivityController {
+  constructor(private readonly activitiesService: ActivitiesService) {}
+
+  @Post()
+  create(@Body() createActivityDto: CreateActivityDto) {
+    return this.activitiesService.create(createActivityDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.activitiesService.findAll();
+  }
+
+  @Get(':id')
+  findAllByGroup(@Param('id') id: string) {
+    return this.activitiesService.findAllByGroup(id);
+  }
+}
